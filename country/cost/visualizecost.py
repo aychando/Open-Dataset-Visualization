60634a938a9023bd8dc7b462b875fd2fe6a63bf0
HUSEIN ZOLKEPLI <husein.zol05@gmail.com>
2017-04-06 00:17:55 -0400

    Add files via upload

---
+++ country/cost/visualizecost.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(25,10))
+
+sns.set(style="whitegrid", palette="muted")
+
+dataset = pd.read_csv('/home/husein/visualization/country/movehubcostofliving.csv')
+
+cost = pd.melt(dataset, "City", var_name="Attributes")
+
+swarm_plot = sns.swarmplot(x="Attributes", y="value", hue="City", data=cost)
+box = swarm_plot.get_position()
+swarm_plot.set_position([box.x0 - 0.09, box.y0, box.width * 0.8, box.height])
+plt.legend(bbox_to_anchor=(1.05, 1.08), loc=2, borderaxespad=0., ncol=5)
+
+fig = swarm_plot.get_figure()
+fig.savefig("cost.png")
+fig.savefig("cost.pdf")
